5a83267153ae7b4a9d9388044dd3723b282ee0fd
Alex Pakka <alex.pakka@centro.net>
2020-02-19 10:27:17 -0600

    Cypress upgrade to 3.8.3; test-e2e fixes

--- test-e2e/cypress/integration/archean/archean_page_spec.js
+++ test-e2e/cypress/integration/archean/archean_page_spec.js
@@ -2,18 +2,24 @@
 describe('Archean Monitor page opens', function() {
     it('successfully loads', function() {
 
-        cy.visit('http://api-gateway:3000/archean/')
+        //redirect web console.logs to cypress console
+        Cypress.on(`window:before:load`, win => {
+            cy.stub( win.console, `log`, msg => {
+                cy.now(`task`, `info`, msg );
+            });
+        });
 
-        cy.get('.nav-wrapper').contains('Overview').click()
-        cy.wait(250)
+        cy.visit('http://api-gateway:3000/archean/')
 
         cy.get('.nav-wrapper').contains('Monitor').click()
         cy.get('.dashboard').contains('Deployments')
+        cy.screenshot()
+
 
         cy.get('.nav-wrapper').contains('E2E Tests').click()
         cy.get('.dashboard .card-tabs').contains('Logs').click()
+        cy.screenshot()
         cy.get('.dashboard .card-tabs').contains('Videos').click()
         cy.get('.dashboard .card-tabs').contains('Screenshots').click()
-        cy.screenshot()
     })
 })
--- test-e2e/cypress/integration/hello_micro_spec.js
+++ test-e2e/cypress/integration/hello_micro_spec.js
@@ -1,23 +1,23 @@
 //note that Cypress runs inside the cluster, so, we use internal service hostname and port as per service.yaml
 
-describe('Test1 app works', function () {
+describe('Hello app works', function () {
 
     beforeEach(() => {
-        cy.request('http://api-gateway:3000/test1').as('test1')
+        cy.request('http://api-gateway:3000/hello').as('hello')
     })
 
-    it('It returns lists of users', function () {
-        cy.get('@test1')
+    it('returns JSON', function () {
+        cy.get('@hello')
             .its('headers')
             .its('content-type')
             .should('include', 'application/json')
     })
 
     it('returns hello property', function () {
-        cy.get('@test1')
+        cy.get('@hello')
             .its('body')
-            .its('greeting')
-            .should('equal', 'Hello from Test ABC1!')
+            .its('hello')
+            .should('not.equal', '')
             .should('not.be.null')
     })
 })
